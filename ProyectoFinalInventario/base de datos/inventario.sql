fe578c4378918c8438ad5dcc4f5a8b3d7045b7c8
M2rt1N7 <74304811+M2rt1N7@users.noreply.github.com>
2021-06-04 10:21:07 +0200

    .

--- ProyectoFinalInventario/base de datos/inventario.sql
+++ ProyectoFinalInventario/base de datos/inventario.sql
@@ -5,6 +5,9 @@ create table usuario(
     contrasenia varchar(100)
 );
 
+INSERT INTO usuario (nombre,contrasenia)
+VALUES ('fede','12345678');
+
 create table producto(
   nombre varchar(100) ,
   codigo varchar(100) primary key,
