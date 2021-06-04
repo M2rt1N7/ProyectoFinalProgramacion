2d866a0b7d2ad6eff5d7509fcbd2f2e4ccb9e735
M2rt1N7 <74304811+M2rt1N7@users.noreply.github.com>
2021-06-04 09:02:50 +0200

    .

--- ProyectoFinalInventario/src/main/java/main/Main.java
+++ ProyectoFinalInventario/src/main/java/main/Main.java
@@ -2,6 +2,11 @@ package main;
 
 import interfaces.Ventana;
 
+/**
+ * Clase principal que ejecuta la clase ventana
+ * @author Fede
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
